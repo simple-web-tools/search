df40abc70bc6fea8b62611e99dcf648ac567b4e8
cjm <ccn@cuppajoeman.com>
2026-08-22 21:57:14 -0400

    feat(search): open text files in viewer

--- search_callbacks.js
+++ search_callbacks.js
@@ -1,5 +1,43 @@
 // Function to handle opening the selected item
 function on_select_callback(item) {
     console.log("Opened:", item.textContent);  // Placeholder callback for when Enter is pressed
-    window.location.href = '/' + item.textContent
+    const path = '/' + item.textContent;
+    const textFileExtensions = [
+        ".bat",
+        ".c",
+        ".cc",
+        ".cfg",
+        ".cmake",
+        ".conf",
+        ".cpp",
+        ".cs",
+        ".css",
+        ".csv",
+        ".frag",
+        ".glsl",
+        ".h",
+        ".hpp",
+        ".ini",
+        ".jai",
+        ".js",
+        ".json",
+        ".lua",
+        ".md",
+        ".ps1",
+        ".py",
+        ".sh",
+        ".txt",
+        ".vert",
+        ".vim",
+        ".xml",
+        ".yml",
+        ".yaml",
+    ];
+
+    if (!path.endsWith(".html") && textFileExtensions.some(extension => path.endsWith(extension))) {
+        window.location.href = "/file_viewer.html?path=" + encodeURIComponent(path);
+        return;
+    }
+
+    window.location.href = path;
 }
